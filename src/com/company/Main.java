2f0fc9823dd574e79f29bd624a4a5193874782d8
Alexander Rogov <mlechnyput1@gmail.com>
2020-10-11 13:36:15 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the string :");
+        String str = scanner.nextLine();
+        System.out.print("Enter the number to repeat the string :");
+        int num = scanner.nextInt();
+        repeat(str,num);
+    }
+
+    public static void repeat(String str, int num){
+        if (num==0)return;
+        num=num-1;
+        System.out.print(str);
+        repeat(str,num);
+    }
+
+}
+
