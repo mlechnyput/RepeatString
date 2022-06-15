a59e405b28701af518005badd855591beadee2b8
mlechnyput <mlechnyput1@gmail.com>
2022-06-16 00:52:03 +0300

    2 commit to THIRD branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,15 +10,18 @@ public class Main {
         String str = scanner.nextLine();
         System.out.print("Enter the number to repeat the string :");
         int num = scanner.nextInt();
-        repeat(str,num);
+        repeat(str, num);
     }
 
-    public static void repeat(String str, int num){
-        if (num==0)return;
-        num=num-1;
+    public static void repeat(String str, int num) {
+        if (num == 0) return;
+        num = num - 1;
         System.out.print(str);
-        repeat(str,num);
+        repeat(str, num);
     }
 
+    private int ret() {
+        return 5;
+    }
 }
 
