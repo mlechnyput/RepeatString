97bfa79fdc7a9c6525ab86f10f60b4a909920082
mlechnyput <mlechnyput1@gmail.com>
2022-06-16 00:14:22 +0300

    second commit to the SECOND branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,18 +10,22 @@ public class Main {
         String str = scanner.nextLine();
         System.out.print("Enter the number to repeat the string :");
         int num = scanner.nextInt();
-        repeat(str,num);
+        repeat(str, num);
     }
 
-    public static void repeat(String str, int num){
-        if (num==0)return;
-        num=num-1;
+    public static void repeat(String str, int num) {
+        if (num == 0) return;
+        num = num - 1;
         System.out.print(str);
-        repeat(str,num);
+        repeat(str, num);
     }
 
-    public static String recursive(){
+    public static String recursive() {
         return "";
+    }
+
+    public void justP() {
+        System.out.println("P");
     }
 
 }
